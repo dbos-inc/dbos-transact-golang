3130bf57bdc4c8ad88595ba63de7f90a4ea5963b
maxdml <maxdml@dbos.dev>
2025-09-03 09:43:48 -0700

    try out versioning for go install

--- cmd/dbos/version.go
+++ cmd/dbos/version.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"runtime/debug"
+
 	"github.com/spf13/cobra"
 )
 
-// Version will be set at build time using ldflags
-var Version = "dev"
+var (
+	Version = "dev" // overridden by -ldflags in CI releases
+	Commit  = ""
+	BuiltAt = ""
+)
+
+func init() {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		// If built via `go install module/cmd@vX.Y.Z`, use the module version.
+		if Version == "dev" && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			Version = info.Main.Version
+		}
+		for _, s := range info.Settings {
+			switch s.Key {
+			case "vcs.revision":
+				if Commit == "" {
+					if len(s.Value) >= 7 {
+						Commit = s.Value[:7]
+					} else {
+						Commit = s.Value
+					}
+				}
+			case "vcs.time":
+				if BuiltAt == "" {
+					BuiltAt = s.Value
+				}
+			case "vcs.modified":
+				if s.Value == "true" && Commit != "" {
+					Commit += "-dirty"
+				}
+			}
+		}
+	}
+}
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
@@ -13,9 +47,8 @@ var versionCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if jsonOutput {
 			return outputJSON(map[string]string{"version": Version})
-		} else {
-			logger.Info("DBOS CLI version", "version", Version)
 		}
+		logger.Info("DBOS CLI version", "version", Version, "commit", Commit, "built", BuiltAt)
 		return nil
 	},
 }
