b61024d231bfbe36429ed83fbac764df50565675
maxdml <maxdml@dbos.dev>
2026-02-19 10:32:20 -0800

    migration default

--- dbos/migrations/8_add_parent_workflow_id.sql
+++ dbos/migrations/8_add_parent_workflow_id.sql
@@ -2,6 +2,6 @@
 -- This enables tracking child workflow lineage for workflows spawned from within other workflows.
 
 ALTER TABLE %s.workflow_status
-ADD COLUMN parent_workflow_id TEXT;
+ADD COLUMN parent_workflow_id TEXT DEFAULT NULL;
 
 CREATE INDEX "idx_workflow_status_parent_workflow_id" ON %s."workflow_status" ("parent_workflow_id");
